cd2cbed8eaed5cc4babc5b6b10b220206b1bb6e5
Petrik Diak <diak@petrik.hu>
2023-12-14 10:15:21 +0100

    Add project files.

---
+++ Etlap/Felvetel.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Etlap
+{
+	/// <summary>
+	/// Interaction logic for Felvetel.xaml
+	/// </summary>
+	public partial class Felvetel : Window
+	{
+		private FoodService foodService;
+		private Food food;
+		public Felvetel(FoodService foodService)
+		{
+			InitializeComponent();
+			this.foodService = foodService;
+		}
+
+		public Felvetel(FoodService foodService, Food food)
+		{
+			InitializeComponent();
+			this.foodService = foodService;
+			this.food = food;
+			this.btnadd.Visibility = Visibility.Collapsed;
+
+			tbname.Text = food.Name;
+			tbdesc.Text = food.Description;
+			cbcat.Text = food.Category;
+			tbprice.Text = food.Price.ToString();
+		}
+
+		private void Add_Button(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				Food food = CreateFoodFromInputData();
+				if (foodService.Create(food))
+				{
+					MessageBox.Show("Sikeres hozzáadás");
+					tbname.Text = "";
+					tbdesc.Text = "";
+					tbprice.Text = "";
+				}
+				else
+				{
+					MessageBox.Show("Hiba történt a hozzáadás során");
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private Food CreateFoodFromInputData()
+		{
+			string name = tbname.Text.Trim();
+			string description = tbdesc.Text.Trim();
+			string category = cbcat.Text.Trim();
+			string price = tbprice.Text.Trim();
+
+			if (string.IsNullOrEmpty(name))
+			{
+				throw new Exception("Név megadása kötelező");
+			}
+
+			if (string.IsNullOrEmpty(price))
+			{
+				throw new Exception("Ár megadása kötelező");
+			}
+			if (!int.TryParse(price, out int age))
+			{
+				throw new Exception("Ár csak szám lehet");
+			}
+
+
+			Food food = new Food();
+			food.Name = name;
+			food.Description = description;
+			food.Category = category;
+			food.Price = Convert.ToInt32(price);
+			return food;
+		}
+	}
+}
---
+++ Etlap/FoodService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Etlap
+{
+	public class FoodService
+	{
+		MySqlConnection connection;
+		public FoodService()
+		{
+			MySqlConnectionStringBuilder builder= new MySqlConnectionStringBuilder();
+			builder.Server = "localhost";
+			builder.Port = 3306;
+			builder.UserID = "root";
+			builder.Password = "";
+			builder.Database = "etlapdb";
+
+			connection = new MySqlConnection(builder.ConnectionString);
+		}
+
+		public bool Create(Food food)
+		{
+			OpenConnection();
+			string sql = "INSERT INTO etlap(nev,leiras,ar,kategoria) VALUES (@name,@desc,@price,@category)";
+			MySqlCommand command = connection.CreateCommand();
+			command.CommandText = sql;
+			command.Parameters.AddWithValue("@name", food.Name);
+			command.Parameters.AddWithValue("@desc", food.Description);
+			command.Parameters.AddWithValue("@price", food.Price);
+			command.Parameters.AddWithValue("@category", food.Category);
+
+			int affectedRows = command.ExecuteNonQuery();
+
+			CloseConnection();
+			return affectedRows == 1;
+
+		}
+
+		public List<Food> GetAll()
+		{
+			List<Food> foods = new List<Food>();
+			OpenConnection();
+			string sql = "SELECT * FROM etlap";
+			MySqlCommand command = connection.CreateCommand();
+			command.CommandText = sql;
+			using (MySqlDataReader reader = command.ExecuteReader())
+			{
+				while (reader.Read())
+				{
+					Food food = new Food();
+					food.Id = reader.GetInt32("id");
+					food.Name = reader.GetString("nev");
+					food.Description = reader.GetString("leiras");
+					food.Price = reader.GetInt32("ar");
+					food.Category = reader.GetString("kategoria");
+					foods.Add(food);
+				}
+			}
+			CloseConnection();
+			return foods;
+		}
+
+		public bool Update(int id, Food newValues)
+		{
+			OpenConnection();
+			string sql = @"UPDATE etlap
+                            SET nev = @name, 
+                                leiras = @desc, 
+                                ar = @price, 
+                                kategoria = @cat 
+                            WHERE id = @id";
+			MySqlCommand command = connection.CreateCommand();
+			command.CommandText = sql;
+			command.Parameters.AddWithValue("@name", newValues.Name);
+			command.Parameters.AddWithValue("@desc", newValues.Description);
+			command.Parameters.AddWithValue("@price", newValues.Price);
+			command.Parameters.AddWithValue("@cat", newValues.Category);
+			command.Parameters.AddWithValue("@id", id);
+			int affectedRows = command.ExecuteNonQuery();
+			CloseConnection();
+			return affectedRows == 1;
+		}
+
+		public bool Delete(int id)
+		{
+			OpenConnection();
+			string sql = "DELETE FROM etlap WHERE id = @id";
+			MySqlCommand command = connection.CreateCommand();
+			command.CommandText = sql;
+			command.Parameters.AddWithValue("@id", id);
+			int affectedRows = command.ExecuteNonQuery();
+			CloseConnection();
+			return affectedRows == 1;
+		}
+
+		private void CloseConnection()
+		{
+			if (connection.State == System.Data.ConnectionState.Open)
+			{
+				connection.Close();
+			}
+		}
+
+		private void OpenConnection()
+		{
+			if (connection.State != System.Data.ConnectionState.Open)
+			{
+				connection.Open();
+			}
+		}
+	}
+}
